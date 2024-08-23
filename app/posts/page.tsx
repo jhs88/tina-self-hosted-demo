837ca9d7cb6ffce7b27e90303a47435ddbfd4776
Joseph Scherreik <joe.scherreik@gmail.com>
2024-08-23 16:35:03 -0400

    update configs

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,5 +3,11 @@ import { client } from "../../tina/__generated__/databaseClient";
 
 export default async function PostList() {
   const res = await client.queries.postConnection();
-  return null;
+  return (
+    <Content
+      data={JSON.parse(JSON.stringify(res.data))}
+      query={res.query}
+      variables={res.variables}
+    />
+  );
 }
