f908a86f301b40f5e766d77971a485dd4ad40dd4
Joseph Scherreik <jscherreik@klishgroup.com>
2024-08-23 14:03:17 -0400

    remove posts lists

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,11 +3,5 @@ import { client } from "../../tina/__generated__/databaseClient";
 
 export default async function PostList() {
   const res = await client.queries.postConnection();
-  return (
-    <Content
-      data={JSON.parse(JSON.stringify(res.data))}
-      query={res.query}
-      variables={res.variables}
-    />
-  );
+  return null;
 }
