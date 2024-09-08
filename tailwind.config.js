7c7c7166efcbbe8cb65a66e9e914a854fb034a43
Joseph Scherreik <joe.scherreik@gmail.com>
2024-09-08 04:04:40 -0400

    fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -86,5 +86,3 @@ const config = {
   },
   plugins: [require("tailwindcss-animate")],
 };
-
-export default config;
